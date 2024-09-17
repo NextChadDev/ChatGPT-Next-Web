87a116937ff206321e2e78f4bf8ad8ae8df1ae67
NextChadDev <chad.dev@lura.care>
2024-09-17 15:34:17 +0200

    changes

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -167,8 +167,9 @@ export function SideBarHeader(props: {
   subTitle?: string | React.ReactNode;
   logo?: React.ReactNode;
   children?: React.ReactNode;
+  shouldNarrow: boolean;
 }) {
-  const { title, subTitle, logo, children } = props;
+  const { title, subTitle, logo, children, shouldNarrow } = props;
   return (
     <Fragment>
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
@@ -223,6 +224,7 @@ export function SideBar(props: { className?: string }) {
         title="NextChat"
         subTitle="Build your own AI assistant."
         logo={<ChatGptIcon />}
+        shouldNarrow={shouldNarrow}
       >
         <div className={styles["sidebar-header-bar"]}>
           <IconButton
--- app/consts/default-model.ts
+++ app/consts/default-model.ts
@@ -1,1 +1,1 @@
-export const DEFAULT_MODEL = "gpt-4o-2024-05-13";
+export const DEFAULT_MODEL = "gpt-4o-2024-08-06";
